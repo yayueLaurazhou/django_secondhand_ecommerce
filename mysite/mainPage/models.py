209a225ef0ec57ffeab6fec1658bb15391685e14
laura <yayuezhou2020@gmail.coom>
2024-02-26 17:22:56 +0000

    initial commit

---
+++ mysite/mainPage/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+class User(models.Model):
+	name = models.CharField(max_length=50)
+	phone = models.CharField(max_length=10)
+	email = models.EmailField(max_length=100)
+	password = models.CharField(max_length=100)
+
+
+	def __str__(self):
+		return f'{self.name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    location = models.CharField(max_length = 10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    created_by = models.ForeignKey(User, related_name ='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
