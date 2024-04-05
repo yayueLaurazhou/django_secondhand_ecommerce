ab34f11b53bc9897e372ea2551d0e73a7483e0ba
laura <yayuezhou2020@gmail.coom>
2024-04-05 11:08:11 +0100

    change settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,11 +132,6 @@ STATIC_URL = "static/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-STATICFILES_DIRS = BASE_DIR / 'static'
-STATIC_ROOT = BASE_DIR /'staticfiles_build'/'static'
-
- 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
