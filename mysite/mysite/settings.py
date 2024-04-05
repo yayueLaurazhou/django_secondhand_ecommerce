e5484cf3de7115b72f7fee1d537ad15f1a2b2d4a
laura <yayuezhou2020@gmail.coom>
2024-04-05 11:03:50 +0100

    render deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
