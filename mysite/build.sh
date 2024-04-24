d1c854a299074ed2c4c8cae6ba1dcde917f40c16
laura <yayuezhou2020@gmail.coom>
2024-04-24 20:43:33 +0100

    add superuser

--- mysite/build.sh
+++ mysite/build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-django-admin createsuperuser --noinput
+python manage.py createsuperuser --noinput
